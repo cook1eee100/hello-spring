dfd6e3ff88e4c779b709edff8740bcf5bcd4acda
cook1eee100 <kathyi@outlook.kr>
2021-11-19 23:53:12 +0900

    회원 관리 예제 - 백엔드 개발

---
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -0,0 +1,60 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    /**
+     * 회원 가입
+     * @return
+     */
+    public Long join(Member member){
+        /**
+         *
+        // 같은 이름이 있는 중복 회원X
+        Optional<Member> result = memberRepository.findByName(member.getName());
+        Member member1 = result.get();// 그냥 값 꺼내고 싶을 때  권장하지는 않음, 대신 orElseget?
+        result.ifPresent(m->{
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        });
+
+        */
+
+        // 같은 이름이 있는 중복 회원X
+        validateDuplicateMember(member); // 중복 회원 검증
+        memberRepository.save(member);
+        return member.getId();
+
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m->{
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    /**
+     * 전체 회원 조회
+     *
+     * @return
+     */
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId){
+        return memberRepository.findById(memberId);
+    }
+}
